fec81a2ef37afb3f8e7ff7c55c0ea0542ca7fa23
vismay-vachhani <vismayvachhani@gmail.com>
2026-07-13 19:04:27 +0530

    Fix pub.dev scoring issues

--- image-crop-compress/publish_all.sh
+++ image-crop-compress/publish_all.sh
@@ -7,7 +7,7 @@ git commit -m 'Fix pub.dev scoring issues' || true
 git push || true
 
 for pkg in image_crop_compress_core image_crop_compress_ui image_crop_compress_android image_crop_compress_ios image_crop_compress; do
-  echo "🚀 Publishing $pkg 0.0.2..."
+  echo "🚀 Publishing $pkg 0.0.3..."
   cd $pkg
   fvm flutter pub publish --force
   cd ..
